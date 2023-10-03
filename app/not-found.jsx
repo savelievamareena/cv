fb5f5a280208e25cd0203f4b79e99fc884856413
savelievamareena <savelieva.mareena.1.21@gmail.com>
2023-10-03 12:50:15 +0300

    Not found page

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFound() {
+    return(
+        <main className="text-center">
+            <h2 className="text-3xl mt-20 mb-4">
+                This page doesn't exist.
+            </h2>
+            <Link href="/" className="underline">Go back to the main page.</Link>
+        </main>
+    )
+}
