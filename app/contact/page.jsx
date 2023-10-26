222f5e4b6f6ce4f8739be6bc8369a76272f55d1d
savelievamareena <savelieva.mareena.1.21@gmail.com>
2023-10-26 14:08:21 +0300

    Minor refactoring

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,7 @@ import ContactForm from "@/app/contact/ContactForm";
 
 export default function ContactMe() {
     return(
-        <div className="lg:w-1/2 sm:w-95 text-center">
+        <div className="lg:w-1/2 w-95 text-center">
             <h2>Want to contact me?</h2>
             <ContactForm/>
         </div>
