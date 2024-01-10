943b1b426f4daf79f6b00a1d75f9efd9c6705ab3
savelievamareena <savelieva.mareena.1.21@gmail.com>
2024-01-10 15:02:31 +0100

    Download button layout improvements

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Hello from "@/app/components/Hello";
 
 export default function Home() {
     return (
-        <main className="text-2xl h-full">
+        <main className="text-2xl h-full flex flex-col w-2/3 xl:w-3/4 justify-center">
             <Hello/>
         </main>
     );
